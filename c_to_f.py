68886825dcd54a1d5a2b51500e23ebeda85cb6da
Rosie Chen <4everstayhungry@gmail.com>
2022-03-20 09:25:54 +0800

    add new file

---
+++ c_to_f.py
@@ -0,0 +1,9 @@
+temperature_C = input('請輸入攝氏溫度：')
+temperature_C = float(temperature_C)
+print(temperature_C *9/5+32)
+
+#ans
+c = input('請輸入攝氏溫度： ')
+c = float(c)
+f = c * 9 / 5 + 32
+print('華氏溫度為： ', f)
